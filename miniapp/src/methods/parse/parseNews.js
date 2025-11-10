4a163062370f70f7b2f1685865b66cb2af828141
Denis <dalekseenko8@gmail.com>
2025-11-10 21:26:17 +0300

    create parseNews

---
+++ miniapp/src/methods/parse/parseNews.js
@@ -0,0 +1,21 @@
+// Получение актуальных новостей с fa.ru
+
+const getNews = async () => {
+  try {
+    const response = await fetch("https://www.fa.ru/university/press-center/");
+    const html = await response.text();
+
+    const regex = /<h[1-9] class="news-card__title">([\s\S]*?)<\/h[1-9]>/g;
+    const titles = [];
+    let match;
+    while ((match = regex.exec(html)) !== null) {
+      titles.push(match[1].replace(/<[^>]+>/g, "").trim()); // удаляем HTML внутри
+    }
+
+    return titles;
+  } catch (e) {
+    return e;
+  }
+};
+
+const res = await getNews();
