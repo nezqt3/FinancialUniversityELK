60f1f3ef0eb0de2b2c07786ebafd959cfe3710fb
Denis <dalekseenko8@gmail.com>
2025-11-14 17:45:14 +0300

    add url apis

--- miniapp/src/config/api.js
+++ miniapp/src/config/api.js
@@ -1,5 +1,7 @@
-const DEFAULT_API_URL = "http://localhost:4000";
+const DEFAULT_API_URL = "http://https://max-hackathon-c3tg.vercel.app/";
 
 export const API_BASE_URL =
-  (typeof import.meta !== "undefined" && import.meta.env && import.meta.env.VITE_API_URL) ||
+  (typeof import.meta !== "undefined" &&
+    import.meta.env &&
+    import.meta.env.VITE_API_URL) ||
   DEFAULT_API_URL;
